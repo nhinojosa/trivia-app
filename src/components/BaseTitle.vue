13877d9d92d39861d022ff5bb0a64c6d106cd92a
advancedwebcitp4316@gmail.com <advancedwebcitp4316@gmail.com>
2024-02-06 17:33:33 -0600

    BaseTitle is working

---
+++ src/components/BaseTitle.vue
@@ -0,0 +1,7 @@
+<template>
+    <div class="flex items-center justify-center gap-4">
+        <h1 class="text-6xl font-thin tracking-widest text-slate-700 uppercase">
+            <slot />
+        </h1>
+    </div>
+</template>
