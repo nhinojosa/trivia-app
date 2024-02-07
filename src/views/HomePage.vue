78d93396a2e3608c19cd206bc2edd1e0ac1c789c
advancedwebcitp4316@gmail.com <advancedwebcitp4316@gmail.com>
2024-02-06 18:04:47 -0600

    Categories API is working

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,8 +1,20 @@
 <script setup>
-import { ref } from 'vue'
+import { onMounted, ref } from 'vue'
+import useAPI from '@/composables/useAPI';
 import BaseTitle from '@/components/BaseTitle.vue';
+
+const {categories, getCategories} = useAPI()
+
+onMounted(async () => {
+  await getCategories()
+})
+
 </script>
 
 <template>
   <BaseTitle>TRIVIA APPLICATION</BaseTitle>
+
+  <div v-for="category in categories" key="category.id">
+    {{ category.name }}
+  </div>
 </template>
