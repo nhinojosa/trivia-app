73cac01bd36abaf9881b15e65e62dd5231924501
advancedwebcitp4316@gmail.com <advancedwebcitp4316@gmail.com>
2024-02-08 09:52:45 -0600

    answers and randomized answers done

--- src/views/QuestionPage.vue
+++ src/views/QuestionPage.vue
@@ -7,10 +7,42 @@ import BaseTitle from "@/components/BaseTitle.vue";
 const api = useAPI()
 const question = ref(null)
 const route = useRoute()
+const answers = ref([])
+
+
+
 
 onMounted(async () => {
   question.value = await api.getQuestion(route.params.id)
+
+  answers.value.push({
+    id: answers.value.length,
+    correct: true,
+    answer: question.value.correct_answer
+  })
+
+  question.value.incorrect_answers.map((wrong_answer) => {
+    answers.value.push({
+      id: answers.value.length,
+      correct: false,
+      answer: wrong_answer
+    })
+    
+  })
+  
+  answers.value = shuffle(answers.value)
+  console.log(answers.value)
 })
+
+const shuffle = (array) => {
+
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1)); //random float value between 0 and 1 [0,1) times i + 1
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+
+  return array;
+}
 
 </script>
 
@@ -19,6 +51,8 @@ onMounted(async () => {
   <div v-if="question">
     <BaseTitle>{{  question.category }}</BaseTitle>
     {{ question.question }}
+
+    <div v-html="answer.answer" v-for="answer in answers" :key="answer.id" class=""></div>
   </div>
    <div v-else class="loading">Loading...</div>
 
